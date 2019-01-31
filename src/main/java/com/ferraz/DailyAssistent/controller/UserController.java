63ba33e68dc41593cb2f6bcddc9741c4cfbe549c
LucasFerrazSilva <lucasferrazsilva@hotmail.com>
2019-01-31 21:49:33 -0200

    User CRUD endpoints done

--- src/main/java/com/ferraz/DailyAssistent/controller/UserController.java
+++ src/main/java/com/ferraz/DailyAssistent/controller/UserController.java
@@ -2,7 +2,9 @@ package com.ferraz.DailyAssistent.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,9 +19,32 @@ public class UserController {
 	private UserRepository userRepository;
 	
 	
-	@GetMapping("/alll")
+	@GetMapping("/all")
 	public @ResponseBody Iterable<User> getAll() {
 		return userRepository.findAll();
 	}
+	
+	@PostMapping("/save")
+	public @ResponseBody String save(User entity) {
+		if(entity != null) {
+			userRepository.save(entity);
+			
+			return "User " + entity.getName() + " saved!";
+		}
+		
+		return "Erro";
+	}
+	
+	@DeleteMapping("/remove")
+	public @ResponseBody String remove(User entity) {
+		if(entity != null) {
+			userRepository.delete(entity);
+			
+			return "User " + entity.getName() + " removed!";
+		}
+		
+		return "Erro";
+	}
+	
 	
 }
--- src/main/java/com/ferraz/DailyAssistent/model/User.java
+++ src/main/java/com/ferraz/DailyAssistent/model/User.java
@@ -18,6 +18,12 @@ public class User {
 	
 	@Column(name="NAME")
 	private String name;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@Column(name="PASSWORD")
+	private String password;
 
 	
 	
@@ -36,8 +42,21 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 }
